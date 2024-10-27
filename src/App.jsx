d7cc5242a0116153e0fafcc4500d09531dcd4380
atamaniuksofiia <165192279+atamaniuksofiia@users.noreply.github.com>
2024-10-27 18:59:35 +0200

    add jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/Profile/FriendsList/FriendsList"
+import friends from "./friends.json"
+import Transaction from "./components/Profile/Transaction/Transaction"
+import transaction from "./transaction.json"
 
-function App() {
-  const [count, setCount] = useState(0)
+const user = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308
+  }
+};
 
+
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        image={user.avatar}
+        stats={user.stats}
+      />
+      <FriendList friends={friends} />
+      <Transaction items={transaction} />
     </>
-  )
-}
-
-export default App
+  );
+};
+export default App;
